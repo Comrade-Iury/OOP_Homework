b72e39b6d13ccb634793531efd7305b77f8e96b5
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2024-05-25 23:36:41 +0500

    Add files via upload

---
+++ 03/02.py
@@ -0,0 +1,20 @@
+class ReversedList:
+    def __init__(self, lst: list):
+        self.lst = lst
+        lst1 = self.lst.copy()
+        for i in range(len(lst1)):
+            self.lst[i] = lst1[i-1]
+        lst1.clear()
+
+    def __len__(self):
+        return len(self.lst)
+
+    def __getitem__(self, key):
+        return self.lst[key]
+
+
+# Ваш код
+
+rl = ReversedList([10, 20, 30])
+for i in range(len(rl)):
+    print(rl[i])
