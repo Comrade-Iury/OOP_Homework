455251e5b118de9be68f7353e62723f43cc5ec55
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2024-06-02 20:17:18 +0500

    Create __init__.py

---
+++ Chess/src/__init__.py
@@ -0,0 +1,10 @@
+from .color import Color
+from .board import Board
+from .pawn import Pawn
+from .knight import Knight
+from .king import King
+from .queen import Queen
+from .bishop import Bishop
+from .rook import Rook
+
+__all__ = ['Color', 'Board', 'Pawn', 'Knight', 'King', 'Queen', 'Bishop', 'Rook']
